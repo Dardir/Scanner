f96a96d345cbaeab115d32a5cba30eeb1cfc30f9
Mahmoud Dardir <eng.mahmoud.dardir@gmail.com>
2020-04-11 14:06:04 +0200

    adding useValidateAnyValueInFields

---
+++ src/useValidateAnyValueInFields.js
@@ -0,0 +1,15 @@
+const useValidateAnyValueInFields = (objectToValidate) => {
+    debugger;
+    let nullArray = [];
+    Object.keys(objectToValidate).forEach(key => {
+        if (objectToValidate[key] === null || objectToValidate[key] === '') {
+            nullArray.push(true);
+        } else {
+            nullArray.push(false);
+        }
+
+    })
+    return [nullArray.every(true)];
+}
+
+export default useValidateAnyValueInFields;
