b191b4b56c925f5749b77690d10194142bc0b32b
Mahmoud Dardir <eng.mahmoud.dardir@gmail.com>
2020-04-19 13:23:14 +0200

    Adding header to fileDisplay

--- src/FileDisplay.js
+++ src/FileDisplay.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import './FileSearch.css';
 
-const FileDisplay = ({metadataObj}) =>{
+const FileDisplay = ({ metadataObj,navigateBack }) => {
     return (
-    <div>Displaying file with ID = {metadataObj.id}</div>
+        <div id="DWTcontainer" className="container">
+            <ul className="search-header">
+                <li className="search-header" style={{ paddingLeft: "5px", float: "left" }}>
+                    <button onClick={navigateBack} style={{ width: "133px" }} ><b>رجوع</b></button>
+                </li>
+            </ul>
+            Displaying file with ID = {metadataObj.id}
+        </div>
     )
 }
 
