db9672b90abcca8c7ef651ce2b4a79d82c4f9e35
Mahmoud Dardir <eng.mahmoud.dardir@gmail.com>
2020-04-19 18:06:02 +0200

    Starting to add metadata

--- src/FileDisplay.js
+++ src/FileDisplay.js
@@ -10,17 +10,34 @@ const FileDisplay = ({ metadataObj, navigateBack }) => {
                     <button onClick={navigateBack} style={{ width: "133px" }} ><b>رجوع</b></button>
                 </li>
             </ul>
-            <div  className="divDisplayFile" style={{ borderStyle: "ridge" ,Height: '688px'}}>
+            <div className="divDisplayFile" style={{ borderStyle: "ridge", Height: '688px' }}>
                 <div className="row">
-                    <div className="columnDisplay" style={{Width: '583px', Height: '688px',borderStyle: 'groove',display:'block'}}>
-                            Displaying file of {metadataObj.id}
+                    <div className="columnDisplay" style={{ Width: '583px', Height: '688px', borderStyle: 'groove', display: 'block' }}>
+                        Displaying file of {metadataObj.id}
                     </div>
                     <div className="column">
-                        Displaying Metadata
+                        <div className="row">
+                            <div className="internal-column">
+                                <li style={{ textAlign: "right", listStyleType: "none" }}>
+                                    <label htmlFor="counsulate">
+                                        <p>القنصلية</p>
+                                    </label>
+                                    <input readOnly type="text" id="txt_counsulate" style={{ paddingLeft: "5px" }} name="counsulate" value={metadataObj.properties.counsulate}  />
+                                </li>
+                            </div>
+                            <div className="internal-column">
+                                <li style={{ textAlign: "right", listStyleType: "none" }}>
+                                    <label htmlFor="txt_barcode">
+                                        <p>باركود</p>
+                                    </label>
+                                    <input readOnly type="text" id="txt_barcode" style={{ paddingLeft: "5px" }} name="barcode" value={metadataObj.properties.barcode}  />
+                                </li>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            
+
         </div>
     )
 }
